95426c97959d146655b32521c3302606c4ec5e18
FrienDDDD <friendjunk@gmail.com>
2022-02-19 02:15:04 +0700

    add votetime

--- prebuit.cpp
+++ prebuit.cpp
@@ -448,13 +448,49 @@ void deleteEvent(int i){
 
 void voteTime(int i){
 
+    int number ;
+    string choice ;
+    printEvent(i);
+    cout << "\n" ;
+    cout << "Please enter the number for vote " << events[i].begin << "to" << events[i].end  << endl ;
+    cout << "Press 1 for agree" << endl ;
+    cout << "Press 2 for disagree" << endl ;
+    do{
+        cout << "the number that you choose: " ;  
+        cin >> number ;
+        if(number != 1 && number != 2 ){
+            cout << "Invalid input,Please try again" << endl ;
+        }
+    }while(number != 1 && number != 2);
+    
 
+    if(number == 1){
+        cout << "You vote agree to " << events[i].begin << "to" << events[i].end << endl ;
+        vote1.push_back(1) ;
+    }
+    if(number == 2){
+        cout << "You vote disagree to " << events[i].begin << "to" << events[i].end << endl ;
+        vote2.push_back(2) ;
+    }
+    
+    cout << "Do you want to see the result? (y/n) :" ;
+    cin >> choice ;
+
+    if(choice == "y"){
+        cout << "-------------------------------------------" << endl ;
+        cout << "\t\tVote Results" << endl ;
+        cout << "Votes of agree got " <<  vote1.size() << endl ;
+        cout <<  "Votes of disagree got " << vote2.size() << endl ;
+        cout << "-------------------------------------------" << endl ;
+    }
 }
 
 void voteLocation(int i){
 
     int number ;
     string choice ;
+    printEvent(i);
+    cout << "\n" ;
     cout << "Please enter the number for vote " << events[i].location << endl ;
     cout << "Press 1 for agree" << endl ;
     cout << "Press 2 for disagree" << endl ;
